e549435b0f7780e73536f518d74a41283bc75eb6
alex.tifox <tilnyi.olexander@gmail.com>
2020-06-18 17:43:16 +0200

    filters tested

--- src/main.py
+++ src/main.py
@@ -2,6 +2,8 @@ import signal_analysis
 import file_manipulation as f_man
 import matplotlib.pyplot as plt
 
+import filters
+
 
 def main():
     f_man.generate_wav_from_string("olek")
@@ -12,17 +14,57 @@ def main():
     plt.show()
     signal_analysis.spectrogram(data)
 
-    signal_analysis.add_noise_to_signal(data, "brown", samplerate, 0.15)
-    signal_analysis.add_noise_to_signal(data, "brown", samplerate, 0.5)
-    signal_analysis.add_noise_to_signal(data, "brown", samplerate, 0.85)
+    # signal_analysis.add_noise_to_signal(data, "brown", samplerate, 0.15)
+    noised_signal_b = signal_analysis.add_noise_to_signal(data, "brown", samplerate, 0.5)
+    # signal_analysis.add_noise_to_signal(data, "brown", samplerate, 0.85)
+    #
+    # signal_analysis.add_noise_to_signal(data, "pink", samplerate, 0.15)
+    noised_signal_p = signal_analysis.add_noise_to_signal(data, "pink", samplerate, 0.5)
+    # signal_analysis.add_noise_to_signal(data, "pink", samplerate, 0.85)
 
-    signal_analysis.add_noise_to_signal(data, "pink", samplerate, 0.15)
-    signal_analysis.add_noise_to_signal(data, "pink", samplerate, 0.5)
-    signal_analysis.add_noise_to_signal(data, "pink", samplerate, 0.85)
+    # signal_analysis.add_noise_to_signal(data, "white", samplerate, 0.15)
+    noised_signal_w = signal_analysis.add_noise_to_signal(data, "white", samplerate, 0.5)
+    # signal_analysis.add_noise_to_signal(data, "white", samplerate, 0.85)
 
-    signal_analysis.add_noise_to_signal(data, "white", samplerate, 0.15)
-    signal_analysis.add_noise_to_signal(data, "white", samplerate, 0.5)
-    signal_analysis.add_noise_to_signal(data, "white", samplerate, 0.85)
+    filtered = filters.iir_bp_filter(noised_signal_b, 640, 1550, samplerate)
+    signal_analysis.spectrum(filtered, samplerate)
+    plt.show()
+    filtered = filters.iir_bp_filter(noised_signal_p, 640, 1550, samplerate)
+    signal_analysis.spectrum(filtered, samplerate)
+    plt.show()
+    filtered = filters.iir_bp_filter(noised_signal_w, 640, 1550, samplerate)
+    signal_analysis.spectrum(filtered, samplerate)
+    plt.show()
+
+    filtered = filters.iir_bs_filter(noised_signal_b, 640, 1550, samplerate)
+    signal_analysis.spectrum(filtered, samplerate)
+    plt.show()
+    filtered = filters.iir_bs_filter(noised_signal_p, 640, 1550, samplerate)
+    signal_analysis.spectrum(filtered, samplerate)
+    plt.show()
+    filtered = filters.iir_bs_filter(noised_signal_w, 640, 1550, samplerate)
+    signal_analysis.spectrum(filtered, samplerate)
+    plt.show()
+
+    filtered = filters.fir_bp_filter(noised_signal_b, 640, 1550, samplerate)
+    signal_analysis.spectrum(filtered, samplerate)
+    plt.show()
+    filtered = filters.fir_bp_filter(noised_signal_p, 640, 1550, samplerate)
+    signal_analysis.spectrum(filtered, samplerate)
+    plt.show()
+    filtered = filters.fir_bp_filter(noised_signal_w, 640, 1550, samplerate)
+    signal_analysis.spectrum(filtered, samplerate)
+    plt.show()
+
+    filtered = filters.fir_bs_filter(noised_signal_b, 640, 1550, samplerate)
+    signal_analysis.spectrum(filtered, samplerate)
+    plt.show()
+    filtered = filters.fir_bs_filter(noised_signal_p, 640, 1550, samplerate)
+    signal_analysis.spectrum(filtered, samplerate)
+    plt.show()
+    filtered = filters.fir_bs_filter(noised_signal_w, 640, 1550, samplerate)
+    signal_analysis.spectrum(filtered, samplerate)
+    plt.show()
 
 
 main()
