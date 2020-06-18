1397cbca936ff858fe718affaf926283565262ad
alex.tifox <tilnyi.olexander@gmail.com>
2020-06-18 11:45:26 +0200

    noises added, tested

--- src/main.py
+++ src/main.py
@@ -1,5 +1,6 @@
 import signal_analysis
 import file_manipulation as f_man
+import matplotlib.pyplot as plt
 
 
 def main():
@@ -8,7 +9,20 @@ def main():
     data, samplerate = f_man.read_wav_from_file()
 
     signal_analysis.spectrum(data, samplerate)
+    plt.show()
     signal_analysis.spectrogram(data)
+
+    signal_analysis.add_noise_to_signal(data, "brown", samplerate, 0.15)
+    signal_analysis.add_noise_to_signal(data, "brown", samplerate, 0.5)
+    signal_analysis.add_noise_to_signal(data, "brown", samplerate, 0.85)
+
+    signal_analysis.add_noise_to_signal(data, "pink", samplerate, 0.15)
+    signal_analysis.add_noise_to_signal(data, "pink", samplerate, 0.5)
+    signal_analysis.add_noise_to_signal(data, "pink", samplerate, 0.85)
+
+    signal_analysis.add_noise_to_signal(data, "white", samplerate, 0.15)
+    signal_analysis.add_noise_to_signal(data, "white", samplerate, 0.5)
+    signal_analysis.add_noise_to_signal(data, "white", samplerate, 0.85)
 
 
 main()
